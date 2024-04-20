e620f8fbe21f0e16f2829d6c73ddfbf0bf158f18
Lauri Etelämäki <lauri.etelamaki@gmail.com>
2024-04-20 20:13:50 +0300

    CMake Hello World

---
+++ build/CMakeFiles/InteplasEditor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InteplasEditor.dir/src/main.cpp.o"
+  "CMakeFiles/InteplasEditor.dir/src/main.cpp.o.d"
+  "InteplasEditor"
+  "InteplasEditor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InteplasEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
